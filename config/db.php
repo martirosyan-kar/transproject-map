00bb5f5a8b2026e96b8dfdee420f0a9f2cc00812
Kar Martirosyan <martirosyan.kar@gmail.com>
2017-05-31 00:27:50 -0500

    deleted config/db.php

--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'pgsql:host=transprojectmap_db_1;dbname=trash',
-    'username' => 'postgres',
-    'password' => '123456',
-    'charset' => 'utf8',
-];
