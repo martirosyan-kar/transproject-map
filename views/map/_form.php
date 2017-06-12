6b379133aad2b30c24c98d357ac08e8356f5d7ae
Kar Martirosyan <martirosyan.kar@gmail.com>
2017-06-12 15:59:09 +0400

    made the description field textarea

--- views/map/_form.php
+++ views/map/_form.php
@@ -32,7 +32,7 @@ use yii\widgets\ActiveForm;
         <?= $this->render('//public/_maps'); ?>
     </div>
 
-    <?= $form->field($model, 'description')->textInput() ?>
+    <?= $form->field($model, 'description')->textarea(['rows' => '10']) ?>
 
     <?= $form->field($model, 'image')->textInput() ?>
 
