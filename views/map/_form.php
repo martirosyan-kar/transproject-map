b1e8843b98ec86714af0a41a21d3338bc1b03b76
Kar Martirosyan <martirosyan.kar@gmail.com>
2017-09-05 00:48:09 -0500

    ability to update back to orange pin

--- views/map/_form.php
+++ views/map/_form.php
@@ -14,44 +14,6 @@ use yii\widgets\ActiveForm;
     }
 </style>
 
-<div class="map-migration-form">
-
-    <?php $form = ActiveForm::begin(); ?>
-
-    <?= $form->field($model, 'region')->textInput() ?>
-
-    <?= $form->field($model, 'district')->textInput() ?>
-
-    <?= $form->field($model, 'community')->textInput() ?>
-
-    <?= $form->field($model, 'longitude')->textInput() ?>
-
-    <?= $form->field($model, 'latitude')->textInput() ?>
-
-    <div class="">
-        <?= $this->render('//public/_maps'); ?>
-    </div>
-
-    <?= $form->field($model, 'description')->textarea(['rows' => '10']) ?>
-
-    <?= $form->field($model, 'image')->textInput() ?>
-
-    <?= $form->field($model, 'cleaned')->radio(['label' => 'Cleaned', 'value' => 1, 'uncheck' => null]) ?>
-
-    <?= $form->field($model, 'cleaned')->radio(['label' => 'Should be approved', 'value' => 2, 'uncheck' => null]) ?>
-
-    <?= $form->field($model, 'cleaned')->radio(['label' => 'Main dumpsites', 'value' => 3, 'uncheck' => null]) ?>
-
-    <?= $form->field($model, 'cleaned_image')->textInput() ?>
-
-    <div class="form-group">
-        <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
-    </div>
-
-    <?php ActiveForm::end(); ?>
-
-</div>
-
 <script>
   var newMarker = null;
   function placeMarker(location, map) {
@@ -82,10 +44,23 @@ use yii\widgets\ActiveForm;
 
     var latLng =  {lat: +data.latitude, lng: +data.longitude};
 
+    if(data.cleaned == 1) {
+      var pinIcon = getCleanedMarker();
+    }
+    else if (data.cleaned == 2) {
+      var pinIcon = getApprovedMarker();
+    }
+    else if (data.cleaned == 3) {
+      var pinIcon = getDumpsitesMarker();
+    }
+    else {
+      var pinIcon = getActiveMarker();
+    }
+
     var marker = new google.maps.Marker({
       position: latLng,
       map: map,
-      icon: getActiveMarker()
+      icon: pinIcon
     });
 
     google.maps.event.addListener(map,'click', function(event) {
@@ -93,3 +68,46 @@ use yii\widgets\ActiveForm;
     });
   }
 </script>
+
+
+<div class="map-migration-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'region')->textInput() ?>
+
+    <?= $form->field($model, 'district')->textInput() ?>
+
+    <?= $form->field($model, 'community')->textInput() ?>
+
+    <?= $form->field($model, 'longitude')->textInput() ?>
+
+    <?= $form->field($model, 'latitude')->textInput() ?>
+
+    <div class="">
+        <?= $this->render('//public/_maps'); ?>
+    </div>
+
+    <?= $form->field($model, 'description')->textarea(['rows' => '10']) ?>
+
+    <?= $form->field($model, 'image')->textInput() ?>
+
+    <?= $form->field($model, 'cleaned')->radio(['label' => 'Trash', 'value' => 0, 'uncheck' => null]) ?>
+
+    <?= $form->field($model, 'cleaned')->radio(['label' => 'Cleaned', 'value' => 1, 'uncheck' => null]) ?>
+
+    <?= $form->field($model, 'cleaned')->radio(['label' => 'Should be approved', 'value' => 2, 'uncheck' => null]) ?>
+
+    <?= $form->field($model, 'cleaned')->radio(['label' => 'Main dumpsites', 'value' => 3, 'uncheck' => null]) ?>
+
+    <?= $form->field($model, 'cleaned_image')->textInput() ?>
+
+    <div class="form-group">
+        <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
+
+
